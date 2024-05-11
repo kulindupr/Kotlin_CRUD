8ba9e35ab8f3f4c9d49bcf368e3284d92cf4e0ab
Kulindu Pabasara <148193086+kulindupr@users.noreply.github.com>
2024-05-11 15:41:13 +0530

    sql implementation(NoteDatabaseHeleper)

--- app/src/main/java/com/example/lab4/MainActivity.kt
+++ app/src/main/java/com/example/lab4/MainActivity.kt
@@ -1,13 +1,22 @@
 package com.example.lab4
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.lab4.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        binding.addButton.setOnClickListener{
+            val intent = Intent(this,AddNoteActivity::class.java)
+            startActivity(intent)
+        }
 
-        setContentView(R.layout.activity_main)
     }
 }
