db8963b9ae03eae0dcc18f4b4b0e2a9e56bb78cd
Kulindu Pabasara <148193086+kulindupr@users.noreply.github.com>
2024-05-11 16:05:59 +0530

    note add implementation (working)

--- app/src/main/java/com/example/lab4/AddNoteActivity.kt
+++ app/src/main/java/com/example/lab4/AddNoteActivity.kt
@@ -2,10 +2,30 @@ package com.example.lab4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.lab4.databinding.ActivityAddNoteBinding
+import com.example.lab4.databinding.ActivityMainBinding
 
 class AddNoteActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAddNoteBinding
+    private lateinit var db: NoteDatabaseHeleper
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_note)
+        binding = ActivityAddNoteBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        db = NoteDatabaseHeleper(this)
+
+        binding.saveButton.setOnClickListener{
+            val title = binding.titleEdittext.text.toString()
+            val content = binding.contendEditText.text.toString()
+            val note = Note(0, title, content)
+            db.insertNote(note)
+            finish()
+            Toast.makeText(this,"Note Saved", Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
