be219afeab268c106e1dba103493733f88df1d69
Kulindu Pabasara <148193086+kulindupr@users.noreply.github.com>
2024-05-12 16:30:45 +0530

    search implemented

--- app/src/main/java/com/example/lab4/MainActivity.kt
+++ app/src/main/java/com/example/lab4/MainActivity.kt
@@ -1,8 +1,9 @@
 package com.example.lab4
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SearchView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.lab4.databinding.ActivityMainBinding
 
@@ -11,6 +12,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var db: NoteDatabaseHeleper
     private lateinit var notesAdapter: NotesAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -23,14 +25,30 @@ class MainActivity : AppCompatActivity() {
         binding.noteRecyclerView.adapter = notesAdapter
 
         binding.addButton.setOnClickListener{
-            val intent = Intent(this,AddNoteActivity::class.java)
+            val intent = Intent(this, AddNoteActivity::class.java)
             startActivity(intent)
         }
 
+        // Search functionality
+        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                if (!newText.isNullOrEmpty()) {
+                    val filteredNotes = db.getAllNotes().filter { it.title.contains(newText, true) }
+                    notesAdapter.refreshData(filteredNotes)
+                } else {
+                    notesAdapter.refreshData(db.getAllNotes())
+                }
+                return true
+            }
+        })
     }
+
     override fun onResume() {
         super.onResume()
         notesAdapter.refreshData(db.getAllNotes())
     }
-
 }
